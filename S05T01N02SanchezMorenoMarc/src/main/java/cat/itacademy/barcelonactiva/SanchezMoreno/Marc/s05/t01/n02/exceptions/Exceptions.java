8bb28819c8bbcbfe894fa5d8d0e0e3c422684f6d
Wrennandsix <marcsmoreno@hotmail.com>
2024-02-08 11:15:08 +0100

    exception added

--- S05T01N02SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s05/t01/n02/exceptions/Exceptions.java
+++ S05T01N02SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s05/t01/n02/exceptions/Exceptions.java
@@ -7,26 +7,32 @@ import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.persistence.EntityNotFoundException;
+
 @ControllerAdvice
 public class Exceptions {
-
-	    @ExceptionHandler(EntityNotFoundException.class)
-	    public ResponseEntity<String> handleNotFoundException(EntityNotFoundException ex){
-	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("flower not found with this id");
-	    }
-	    
-	    @ExceptionHandler(HttpMessageConversionException.class)
-	    public ResponseEntity<String> handleInvalidInput(HttpMessageConversionException ex){
-	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect data type");
-	    }
-
-	    @ExceptionHandler(Exception.class)
-	    public ResponseEntity<String> handleGeneralException(Exception ex){
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("unexpected error\n" + ex.getMessage());
-	    }
-	    @ExceptionHandler(EmptyResultDataAccessException.class)
-	    public ResponseEntity<String> handleNoFlowersFound(EmptyResultDataAccessException ex) {
-	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No flowers found in our database. Reason: " + ex.getMessage());
-	    }
+	
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<String> handleNotFoundException(EntityNotFoundException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("flower not found with this id");
 	}
+
+	
+	@ExceptionHandler(HttpMessageConversionException.class)
+	public ResponseEntity<String> handleInvalidInput(HttpMessageConversionException ex) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect data type");
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleGeneralException(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("unexpected error\n" + ex.getMessage());
+	}
+
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	public ResponseEntity<String> handleNoFlowersFound(EmptyResultDataAccessException ex) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body("No flowers found in our database. Reason: " + ex.getMessage());
+	}
+}
